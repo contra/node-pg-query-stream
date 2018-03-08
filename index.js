e8ff7b289e09b0968a85fd3589b1f9d121a1e238
danypype <danypype@gmail.com>
2018-03-08 18:15:08 -0500

    Properly shift objects from _buffer in while loop

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ class PgQueryStream extends Readable {
     var readAmount = Math.max(size, this.batchSize)
     var object
 
-    while (object = this._buffer.shift() && readAmount) {
+    while ((object = this._buffer.shift()) && readAmount) {
       readAmount--;
       if (!this.push(object)) {
         this._reading = false
